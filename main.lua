aa3dc09ecba12447555f35ddd8c0d51cb3c38a05
haxer19 <caotienthanh924@gmail.com>
2025-04-04 18:53:17 +0700

    haoni

--- main.lua
+++ main.lua
@@ -82,12 +82,7 @@ Tabs.Main:Toggle({
     Default = true,
     Callback = function(state)
         if state then
-            local Lighting = game:GetService("Lighting")
-            Lighting.Brightness = 2
-            Lighting.ClockTime = 14
-            Lighting.FogEnd = 100000
-            Lighting.GlobalShadows = false
-            Lighting.OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+            fb:Enable() 
         end
     end
 })
