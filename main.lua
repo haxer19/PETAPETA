b824705b934d46e26ee11696d8479063476d7ec2
haxer19 <caotienthanh924@gmail.com>
2025-04-06 10:21:50 +0700

    up !!

--- main.lua
+++ main.lua
@@ -55,6 +55,8 @@ local Tabs = {
     Player = Window:Tab({ Title = "Player", Icon = "rbxassetid://7734056608", Desc = "player" }),
 }
 
+Window:SelectTab(1)
+
 WindUI:Notify({
     Title = "ESP PETAPETA",
     Content = "Activated !!",
@@ -182,10 +184,9 @@ Tabs.Main:Button({
 -- Functions:
 
 Tabs.Main:Section({ Title = "Level 1" })
--- Tạo button với callback function
 Tabs.Main:Button({
     Title = "Activate the Ofuda",
-    Desc = "",
+    Desc = "key auto-fetched.",
     Callback = function()
         local Character = game.Players.LocalPlayer.Character
         local Box = workspace.Server.SpawnedItems.OfudaBox2.OfudaPoint
@@ -321,7 +322,7 @@ end
 
 Tabs.Main:Button({
     Title = "Hint Paper Finder",
-    Desc = "",
+    Desc = "key auto-fetched.",
     Callback = function()
         local Character = game:GetService("Players").LocalPlayer.Character
         local HumanoidRootPart = Character and Character:FindFirstChild("HumanoidRootPart")
@@ -391,68 +392,45 @@ Tabs.Main:Button({
                             fireproximityprompt(workspace.Server.SpawnedItems.OfudaBox2.InteractPoint.ItemInteractP)
                             task.wait(0.5)
                             fireproximityprompt(workspace.Server.SpawnedItems.Ofuda.Handle.ItemInteractP)
-                            
-                            local finalCFrame = CFrame.new(560.903931, 39.1999626, 602.641663, 0.999947727, 8.53170786e-05, -0.0102250045, 3.32571208e-16, 0.999965191, 0.00834367424, 0.0102253603, -0.00834323838, 0.999912918)
-                            moveToPosition3(HumanoidRootPart, finalCFrame, 2)
-                            task.wait(0.3)
-                            game:GetService("ReplicatedStorage").ItemHandler.OfudaRequest:FireServer(
-                                CFrame.new(518.0667724609375, 41.32714080810547, 609.953125) * 
-                                CFrame.Angles(2.7852535247802734, 0.13421067595481873, -3.0918264389038086)
-                            )
-                            task.wait(0.4)
-                            moveToPosition3(HumanoidRootPart, Box.CFrame, 2)
-                            task.wait(0.5)
-                            if Character.Humanoid:EquipTool(Backpack:WaitForChild("Key")) then
-                                task.wait(0.5) 
-                                fireproximityprompt(workspace.Server.SpawnedItems.OfudaBox2.InteractPoint.ItemInteractP)
+                            task.wait(1)
+                            if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+                                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-122.064453125, 41.4366455078125, 246.3217315673828) * CFrame.Angles(3.0124473571777344, -0.18015220761299133, 3.1183271408081055)
+                                game:GetService("ReplicatedStorage").ItemHandler.OfudaRequest:FireServer(CFrame.new(-122.064453125, 41.4366455078125, 246.3217315673828) * CFrame.Angles(3.0124473571777344, -0.18015220761299133, 3.1183271408081055))
+                                task.wait(0.5)
+                                local plr = game.Players.LocalPlayer
+                                local char = plr.Character or plr.CharacterAdded:Wait()
+                                local hrp = char:WaitForChild("HumanoidRootPart")
+
+                                hrp.CFrame = hrp.CFrame + Vector3.new(0, 78, 0)
+                                task.wait(0.1)
+                                hrp.CFrame = hrp.CFrame + Vector3.new(0, 0, -109)
+
+                                local bp = Instance.new("BodyPosition")
+                                bp.Position = hrp.Position
+                                bp.MaxForce = Vector3.new(0, math.huge, 0)
+                                bp.Parent = hrp
+
+                                local bg = Instance.new("BodyGyro")
+                                bg.MaxTorque = Vector3.new(math.huge, math.huge, math.huge)
+                                bg.CFrame = hrp.CFrame
+                                bg.Parent = hrp
+
+                                hrp.Anchored = true
+                                task.spawn(function()
+                                    while task.wait() do
+                                        for _, v in pairs(char:GetDescendants()) do
+                                            if v:IsA("BasePart") then
+                                                v.CanCollide = false
+                                            end
+                                        end
+                                    end
+                                end)
                             end
-                            task.wait(0.2)
-                            fireproximityprompt(workspace.Server.SpawnedItems.Ofuda.Handle.ItemInteractP)
                         end
                     end
                 end)
                 break
             end
-        end
-    end
-})
-
-Tabs.Main:Button({
-    Title = "Activate the Ofuda",
-    Desc = "",
-    Callback = function()
-        if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-122.064453125, 41.4366455078125, 246.3217315673828) * CFrame.Angles(3.0124473571777344, -0.18015220761299133, 3.1183271408081055)
-            game:GetService("ReplicatedStorage").ItemHandler.OfudaRequest:FireServer(CFrame.new(-122.064453125, 41.4366455078125, 246.3217315673828) * CFrame.Angles(3.0124473571777344, -0.18015220761299133, 3.1183271408081055))
-            task.wait(0.5)
-            local plr = game.Players.LocalPlayer
-            local char = plr.Character or plr.CharacterAdded:Wait()
-            local hrp = char:WaitForChild("HumanoidRootPart")
-
-            hrp.CFrame = hrp.CFrame + Vector3.new(0, 78, 0)
-            task.wait(0.1)
-            hrp.CFrame = hrp.CFrame + Vector3.new(0, 0, -109)
-
-            local bp = Instance.new("BodyPosition")
-            bp.Position = hrp.Position
-            bp.MaxForce = Vector3.new(0, math.huge, 0)
-            bp.Parent = hrp
-
-            local bg = Instance.new("BodyGyro")
-            bg.MaxTorque = Vector3.new(math.huge, math.huge, math.huge)
-            bg.CFrame = hrp.CFrame
-            bg.Parent = hrp
-
-            hrp.Anchored = true
-            task.spawn(function()
-                while task.wait() do
-                    for _, v in pairs(char:GetDescendants()) do
-                        if v:IsA("BasePart") then
-                            v.CanCollide = false
-                        end
-                    end
-                end
-            end)
         end
     end
 })
@@ -635,6 +613,21 @@ Tabs.Visual:Dropdown({
     Value = "",
     Callback = function(option)
         ESP.SkeletonsColor = Colors[option]
+    end
+})
+
+Tabs.Player:Section({ Title = " ", TextXAlignment = "Center" })
+local TL = loadstring(game:HttpGet("https://raw.githubusercontent.com/haxer19/PETAPETA/main/TL"))()
+
+Tabs.Player:Toggle({
+    Title = "Troll Enemy",
+    Default = false,
+    Callback = function(state)
+        if state then
+            TL:StartTroll()
+        else
+            TL:StopTroll()
+        end
     end
 })
 
